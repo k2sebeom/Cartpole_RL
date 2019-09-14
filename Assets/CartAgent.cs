6df8faa13a0c85c054a711619b285f33729e7984
Sebeom Lee <moses97@gmail.com>
2019-09-15 02:48:04 +0900

    first commit

---
+++ Assets/CartAgent.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class CartAgent : Agent
+{
+    public Rigidbody2D PollBody;
+    public Transform PollPos;
+
+    public override void AgentReset()
+    {
+        transform.localPosition = Vector2.zero;
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        PollBody.velocity = Vector2.zero;
+        PollBody.angularVelocity = 0;
+        PollPos.localPosition = new Vector2(0, 3f);
+        PollPos.transform.rotation = Quaternion.Euler(0, 0, 0);
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    public override void CollectObservations()
+    {
+        string result = "AngVel: " + Mathf.Clamp(PollBody.angularVelocity / 500f, -1f, 1f).ToString();
+        AddVectorObs(Mathf.Clamp(PollBody.angularVelocity / 500f, -1f, 1f));
+        result += " / Angle: " + Mathf.Clamp(PollPos.eulerAngles.z / 100f, -1f, 1f).ToString();
+        AddVectorObs(Mathf.Clamp(PollPos.eulerAngles.z / 100f, -1f, 1f));
+        result += " / CartVel: " + Mathf.Clamp(GetComponent<Rigidbody2D>().velocity.x / 10f, -1f, 1f).ToString();
+        AddVectorObs(Mathf.Clamp(GetComponent<Rigidbody2D>().velocity.x / 10f, -1f, 1f));
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        AddReward(0.1f);
+        int action = (int)vectorAction[0];
+        if (action == 1)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-10f, 0f));
+        }
+        else if (action == 2)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(10f, 0f));
+        }
+
+    }
+}
---
+++ Assets/PoleController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleController : MonoBehaviour
+{
+    public CartAgent Cart;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Finish"))
+        {
+            Cart.AddReward(-1f);
+        }
+        Cart.Done();
+    }
+}
